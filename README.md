3a2a0fa8b1a5f613829a484ce54f25b0e4f8ee05
benjamin <benjaminvanseggelen@hotmail.com>
2021-01-12 21:54:09 +0100

    Proper sourcemaps, dart sass instead of node sass

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # kruna_css
+
 A minimal CSS library, mostly for personal use
+
+To run:
+
+> npm run build
+
+If there are any dependencies missing, run:
+
+> npm install
--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,15 @@ const concat = require('gulp-concat');
 const streamqueue = require('streamqueue');
 const sourcemaps = require('gulp-sourcemaps');
 
-sass.compiler = require('node-sass');
+sass.compiler = require('sass'); // Using the dart sass compiler
 
 gulp.task('build', function () {
-	let normalizeStream = src('./vendors/normalize.css').pipe(
-		concat('normalize.css')
-	);
+	let normalizeStream = src('./vendors/normalize.css')
+		.pipe(concat('normalize.css'))
+		.pipe(sourcemaps.init());
 
 	let sassStream = src('./src/scss/**/*.scss')
+		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
 		.pipe(concat('style.scss'));
 
@@ -22,9 +23,8 @@ gulp.task('build', function () {
 		normalizeStream,
 		sassStream
 	)
-		.pipe(sourcemaps.init())
 		.pipe(concat('kruna.css'))
-		.pipe(sourcemaps.write())
+		.pipe(sourcemaps.write('./'))
 		.pipe(dest('dist/'));
 
 	return fullStream;
